a9c10ec84633ee341f2f4ecf0e2ab2636519ddf6
miraggiom-dev <m.mistage@urbe.edu.ve>
2024-10-04 15:29:36 -0400

    update 3

--- codigo3.cpp
+++ codigo3.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
 
     bool SYS = true;
-    string nombre, tecla, N, S;
+    string nombre, tecla, N = "N", S = "S", promedioAL;
     string estudiante_mayor_nota, estudiante_menor_nota, estudiante_mayor_cantidad_notas, estudiante_menor_cantidad_notas;
-    string estudiante_mayor_edad, estudiante_menor_edad;
+    string estudiante_mayor_edad, estudiante_menor_edad, estudiante_mayor_promedio, estudiante_menor_promedio;
     int dia, mes, ano, dia_actual, mes_actual, ano_actual;
     int edad, dia_mayor_edad, dia_menor_edad, mes_mayor_edad, mes_menor_edad, cantidad_estudiantes, cantidad_notas;
     int mayor_edad = 0, menor_edad = 100, cantidad_notas_procesadas = 0, mayor_cantidad_notas = 0, menor_cantidad_notas = 100, cantidad_notas_erroneas = 0;
-    double nota, suma_notas = 0, mayor_nota = 0, menor_nota = 100;
+    double nota, suma_notas = 0, mayor_nota = 0, menor_nota = 100, mayor_promedioV = -1, menor_promedioV = 21, mayor_promedioA = -1, menor_promedioA = 21;
 
     do {
         // Inicializar introduciendo cantidad de estudiantes
@@ -46,7 +46,6 @@ int main() {
 
             cout << "Año: ";
             cin >> ano;
-
 
             cout << "\nIngrese la cantidad de notas: ";
             cin >> cantidad_notas;
@@ -105,33 +104,72 @@ int main() {
                 dia_menor_edad = dia;
                 estudiante_menor_edad = nombre;
             }
+
+            // Calcular promedio
+
+            double promedioV = suma_notas / cantidad_notas;
+            double promedioA = promedioV / 2;
+            string promedioAL;
+
+            if (promedioV > mayor_promedioV) {
+                mayor_promedioV = promedioV;
+                estudiante_mayor_promedio = nombre;
+            }
+            if (promedioV < menor_promedioV) {
+                menor_promedioV = promedioV;
+                estudiante_menor_promedio = nombre;
+            }
+
+            if (promedioA > mayor_promedioA) {
+                mayor_promedioA = promedioA;
+                estudiante_mayor_promedio = nombre;
+            }
+            if (promedioA < menor_promedioA) {
+                menor_promedioA = promedioA;
+                estudiante_menor_promedio = nombre;
+            }
+
+            if (promedioA >= 0 && promedioA < 6) {
+                promedioAL = "F";
+            } else if (promedioA >= 6 && promedioA < 7) {
+                promedioAL = "D";
+            } else if (promedioA >= 7 && promedioA < 8) {
+                promedioAL = "C";
+            } else if (promedioA >= 8 && promedioA < 9) {
+                promedioAL = "B";
+            } else if (promedioA >= 9 && promedioA <= 10) {
+                promedioAL = "A";
+            }
         }
 
         // Resultados
 
+        cout << "\nResultados: " << endl;
+
         cout << "\nEl estudiante con la mayor nota es: " << estudiante_mayor_nota << " con " << mayor_nota << "." << endl;
         cout << "El estudiante con la menor nota es: " << estudiante_menor_nota << " con " << menor_nota << "." << endl;
+        cout << "\nEl estudiante con el mayor promedio es: " << estudiante_mayor_promedio << " con " << mayor_promedioV << ", y en sistema americano: " << mayor_promedioA << " = " << promedioAL << endl;
+        cout << "El estudiante con el menor promedio es: " << estudiante_menor_promedio << " con " << menor_promedioV << ", y en sistema americano: " << menor_promedioA << " = " << promedioAL << endl;
         cout << "\nEl estudiante con la mayor edad es: " << estudiante_mayor_edad << " con " << mayor_edad << " años." << endl;
         cout << "El estudiante de menor edad es: " << estudiante_menor_edad << " con " << menor_edad << " años." << endl;
-        cout << "\nLa cantidad de notas procesadas fue: " << cantidad_notas_procesadas << "."  << endl;
-        cout << "La cantidad de estudiantes procesados fue: " << cantidad_estudiantes << "." << endl;
         cout << "\nEl estudiante con más notas procesadas es: " << estudiante_mayor_cantidad_notas << " con " << mayor_cantidad_notas << " notas." << endl;
         cout << "El estudiante con menos notas procesadas es: " << estudiante_menor_cantidad_notas << " con " << menor_cantidad_notas << " notas." << endl;
-        cout << "\nLa cantidad de notas erróneas fue: " << cantidad_notas_erroneas << "." << endl;
+        cout << "\nLa cantidad de notas procesadas fue: " << cantidad_notas_procesadas << "."  << endl;
+        cout << "La cantidad de estudiantes procesados fue: " << cantidad_estudiantes << "." << endl;
+        cout << "La cantidad de notas erróneas fue: " << cantidad_notas_erroneas << "." << endl;
 
         cout << "Desea continuar? \n No = N, Sí = S. " << endl;
         cin >> tecla;
 
         if(tecla == N) {
             SYS = false;
-        }
-        else {
+        } else {
             SYS = true;
         }
 
         system("pause");
         system("cls");
 
-    } while (SYS == true);
+    } while (SYS);
 
 }
